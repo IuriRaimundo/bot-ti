b4a715cf1a3ea522803c70ff1b9d4fef67d24592
Afonso Santos <afonso.faisca1@gmail.com>
2021-10-31 20:10:33 +0000

    Prettier

--- utils/selectRandomArrayElement.js
+++ utils/selectRandomArrayElement.js
@@ -6,16 +6,19 @@ function getRandomInt(max) {
 
 function selectRandomArrayElement(array) {
     let length, index;
+
     // Type checking
-    if (!Array.isArray(array))
-    {
+    if (!Array.isArray(array)) {
         console.log('Error at function selectRandomArrayElement, the "array" parameter should be of type: Array.');
         return;
     }
+
     // Get array length
     length = array.length;
+
     // Get a random integer between 0 and the length
     index = getRandomInt(length);
+
     // Return a message from the messages array using that random integer
     return array[index];
 }
