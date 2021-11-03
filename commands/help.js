f76ec077053a36648f65ccc40d5f909bc5016066
Raimundo <iuraimundo@deloitte.pt>
2021-11-03 00:09:24 +0000

    moved help embedded message to the messages file

--- commands/help.js
+++ commands/help.js
@@ -1,17 +1,9 @@
-const Discord = require('discord.js');
+'use strict'
 
-const embed = new Discord.MessageEmbed()
-  .setColor('#77185e')
-  .setTitle('Lista de Comandos')
-  .setAuthor('Távora Bot', 'https://github.com/afonsosantos')
-  .setDescription('Lista de comandos disponíveis')
-  .setThumbnail('https://i.imgur.com/2r3EbTF.png')
-  .addField(`**duvida + mensagem**`, 'Responder a dúvidas')
-  .addField(`**ajuda / help**`, 'Mostra esta mensagem')
-  .setTimestamp()
-  .setFooter('Bot por Engenheiros da Deloitte', 'https://i.imgur.com/1LHooWF.png');
+const Discord = require('discord.js');
+const { helpEmbed } = require('../messages');
 
 function help(message) {
-    message.channel.send({ embeds: [embed] });
+    message.channel.send({ embeds: [helpEmbed] });
 }
   module.exports = help;
