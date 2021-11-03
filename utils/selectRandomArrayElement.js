eb0d7cfe2c9ac3cf89c294063cfd431b47741930
Afonso Santos <afonso.faisca1@gmail.com>
2021-11-03 19:12:24 +0000

    Removed dull 'a'

--- utils/selectRandomArrayElement.js
+++ utils/selectRandomArrayElement.js
@@ -4,21 +4,21 @@ const chalk = require('chalk');
 const getRandomInt = require('./getRandomInt');
 
 function selectRandomArrayElement(array) {
-    let length, index;
+  let length, index;
 
-    // Type checking
-    if (!Array.isArray(array)) {
-        throw Error(chalk.red('Error at function selectRandomArrayElement, the "array" parameter should be of type: Array.'));
-    }
+  // Type checking
+  if (!Array.isArray(array)) {
+    throw Error(chalk.red('Error at function selectRandomArrayElement, the "array" parameter should be of type: Array.'));
+  }
 
-    // Get array length
-    length = array.length;
+  // Get array length
+  length = array.length;
 
-    // Get a random integer between 0 and the length
-    index = getRandomInt('a');
+  // Get a random integer between 0 and the length
+  index = getRandomInt(length);
 
-    // Return a message from the messages array using that random integer
-    return array[index];
+  // Return a message from the messages array using that random integer
+  return array[index];
 }
 
 module.exports = selectRandomArrayElement;
